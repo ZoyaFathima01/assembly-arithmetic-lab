d1ce4dec57791134f91818854b377e51cddd8fe3
ZoyaFathima01 <zoyaafathimaa001@gmail.com>
2025-06-26 21:03:03 -0700

    Update eq2.asm

--- eq2.asm
+++ eq2.asm
@@ -1,21 +1,22 @@
 section .data
-    var1    dd  2
-    var2    dd  3
-    var3    dd  4
-    var4    dd  5
-    result  dd  0
+    var1 dd 3
+    var2 dd 4
+    var3 dd 5
+    var4 dd 2
+
+section .bss
+    result resd 1
 
 section .text
     global _start
 
 _start:
-    mov     eax, [var1]
-    add     eax, [var2]
-    add     eax, [var3]
-    add     eax, [var4]
-    mov     [result], eax
+    mov eax, [var1]
+    add eax, [var2]
+    add eax, [var3]
+    add eax, [var4]
+    mov [result], eax
 
     ; exit
-    mov     eax, 1
-    xor     ebx, ebx
-    int     0x80
+    mov eax, 1
+    int 0x80
