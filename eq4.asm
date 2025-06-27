cb6549836d8db72682e8489e5b4dca2fcbbe4f59
ZoyaFathima01 <zoyaafathimaa001@gmail.com>
2025-06-26 21:04:10 -0700

    Update eq4.asm

--- eq4.asm
+++ eq4.asm
@@ -1,20 +1,24 @@
 section .data
-    var1    dd  8
-    var2    dd  7
-    result  dd  0
+    var1 dd 8
+    var2 dd 7
+    two  dd 2
+    three dd 3
+
+section .bss
+    result resd 1
 
 section .text
     global _start
 
 _start:
-    mov     eax, [var1]
-    imul    eax, eax, 2
-    mov     ebx, [var2]
-    sub     ebx, 3
-    cdq
-    idiv    ebx
-    mov     [result], eax
+    mov eax, [var1]
+    imul eax, [two]       ; eax = var1 * 2
+    mov ebx, [var2]
+    sub ebx, [three]      ; ebx = var2 - 3
+    cdq                   ; sign extend eax into edx:eax
+    idiv ebx              ; eax = eax / ebx
+    mov [result], eax
 
-    mov     eax, 1
-    xor     ebx, ebx
-    int     0x80
+    ; exit
+    mov eax, 1
+    int 0x80
